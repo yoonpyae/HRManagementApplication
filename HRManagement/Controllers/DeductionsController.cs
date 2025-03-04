8ebb2995ad69c6ecbcf94c3d8f5e6094b8926c27
ypeain <yoonpyae26@gmail.com>
2025-03-04 09:43:30 +0630

    add get all deduction

---
+++ HRManagement/Controllers/DeductionsController.cs
@@ -0,0 +1,39 @@
+﻿using HRManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeductionsController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext _context = context;
+
+        [HttpGet]
+        [EndpointSummary("Get all Deduction")]
+        [EndpointDescription("Get all Deductions")]
+           public async Task<IActionResult> GetHrDections()
+        {
+            var deductions= await _context.ViHrDeductions.Where(x=>!x.DeletedOn.HasValue).ToListAsync();
+            return deductions != null
+                ? Ok(new DefaultResponseModel()
+                {
+                    Success = true,
+                    StatusCode = StatusCodes.Status200OK,
+                    Data = deductions,
+                    Message = "Deductions fetched successfully!"
+                }
+                ) : BadRequest(new DefaultResponseModel()
+                {
+                    Success = false,
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Data = deductions,
+                    Message = "No Deductions found"
+
+                });
+
+        }
+    }
+}
