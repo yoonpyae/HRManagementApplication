2451f57abef2e5aaaae76cfd566132ce547ca03a
ypeain <yoonpyae26@gmail.com>
2025-03-07 13:32:48 +0630

    add employee controller

---
+++ HRManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using HRManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext _context=context;
+
+    }
+}
