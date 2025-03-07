280be195b5828da1f9ee371c87a3730e4f4c09a0
ypeain <yoonpyae26@gmail.com>
2025-03-07 13:41:15 +0630

    add get all employee

--- HRManagement/Controllers/EmployeesController.cs
+++ HRManagement/Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 ﻿using HRManagement.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HRManagement.Controllers
 {
@@ -9,6 +10,30 @@ namespace HRManagement.Controllers
     public class EmployeesController(AppDbContext context) : ControllerBase
     {
         private readonly AppDbContext _context=context;
+
+        #region Get Employee
+        [HttpGet]
+        [EndpointSummary("Get all Employees")]
+        public async Task<IActionResult> GetEmployee()
+        {
+            var employee= await _context.ViHrEmployees.ToListAsync();
+            return employee!=null
+                ? Ok (new DefaultResponseModel()
+                {
+                    Success = true,
+                    StatusCode=StatusCodes.Status200OK,
+                    Data=employee,
+                    Message="Success"
+                })
+                : BadRequest(new DefaultResponseModel()
+                {
+                    Success = false,
+                    StatusCode=StatusCodes.Status400BadRequest,
+                    Data=null,
+                    Message="Failed"
+                });
+        }
+        #endregion
 
     }
 }
